6926f51d45effb5e673ec47e8d6d1cf6c54718d3
LIKHITHA-NAGRAJ <nagarajucnagarajuc7.in>
2025-06-28 14:10:27 +0530

    sum

---
+++ 1st.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,sum=0;
+    printf("Enter number : ");
+    scanf("%d %d",&num1,&num2);
+    sum=num1+num2;
+    printf("Sum : %d ",sum);
+    return 0;
+}
